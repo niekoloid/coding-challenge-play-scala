a76d15d226a209b2afbebf6ba392caf427db8e15
sunsagong <sagong.sun@gmail.com>
2016-09-15 15:25:34 +0800

    Add transaction model template

---
+++ app/models/Transaction.scala
@@ -0,0 +1,17 @@
+package models
+
+import play.api.db._
+import anorm._
+import play.api.Play.current
+import anorm.SqlParser._
+import play.api.db.DB
+
+
+
+case class Transaction extends models{
+
+}
+
+object AddressBook {
+
+}
